ceb2a7d2039bd005c64f1a90ec293316fb414874
KhohZongEu <74801595+KhohZongEu@users.noreply.github.com>
2022-01-16 09:52:02 +0800

    Add files via upload

---
+++ water_level.ino
@@ -0,0 +1,60 @@
+// Sensor pins
+#define sensorPower 7
+#define sensorPin A0
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width,  in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// declare an SSD1306 display object connected to I2C
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// Value for storing water level
+int val = 0;
+
+void setup() {
+  pinMode(sensorPower, OUTPUT);// Set D7 as an OUTPUT
+  digitalWrite(sensorPower, LOW);// Set to LOW so no power flows through the sensor
+  Serial.begin(9600);
+
+  // initialize OLED display with address 0x3C for 128x64
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    while (true);
+  }
+}
+
+void loop() {
+  //get the reading from the function below and print it
+  int level = readSensor();
+
+  Serial.print("Water level: ");
+  Serial.println(level);
+  oled.clearDisplay(); // clear display
+  oled.setTextSize(1);          // text size
+  oled.setTextColor(WHITE);
+  oled.setCursor(0, 0);       // position to display
+  oled.println("Water level:");// text color
+  oled.setCursor(0, 30);       // position to display
+  oled.println(level); // text to display
+  oled.display();
+
+  
+  delay(1000);
+}
+
+//This is a function used to get the reading
+int readSensor() {
+  digitalWrite(sensorPower, HIGH);  // Turn the sensor ON
+  delay(10);              // wait 10 milliseconds
+  val = analogRead(sensorPin);    // Read the analog value form sensor
+  digitalWrite(sensorPower, LOW);   // Turn the sensor OFF
+  return val;             // send current reading
+
+
+
+  
+}
